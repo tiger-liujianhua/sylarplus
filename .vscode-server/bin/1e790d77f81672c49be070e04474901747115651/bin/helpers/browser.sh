72bd6440ad6c342652c691d86ef9fde07cb06931
tiger-liujianhua <liujianhuatust@163.com>
2024-03-11 14:30:08 +0800

    init

---
+++ .vscode-server/bin/1e790d77f81672c49be070e04474901747115651/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="code"
+VERSION="1.87.1"
+COMMIT="1e790d77f81672c49be070e04474901747115651"
+EXEC_NAME="code"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
